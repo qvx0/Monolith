0c2f387345efdbcd02b93f242327a5d14adeb16c
velocity <113923623+qvx0@users.noreply.github.com>
2024-03-10 16:49:40 +0100

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -253,7 +253,7 @@ end
         local TabButton = library:create("TextButton", {
             BackgroundColor3 = Color3.fromRGB(255, 255, 255),
             BackgroundTransparency = 1,
-            Size = UDim2.new(0, 76, 0, 90),
+            Size = UDim2.new(0, 89, 0, 86),
             Text = "",
         }, TabButtons)
 
@@ -261,7 +261,7 @@ end
             AnchorPoint = Vector2.new(0.5, 0.5),
             BackgroundTransparency = 1,
             Position = UDim2.new(0.5, 0, 0.5, 0),
-            Size = UDim2.new(0, 35, 0, 35),
+            Size = UDim2.new(0, 89, 0, 86),
             Image = tab_image,
             ImageColor3 = Color3.fromRGB(100, 100, 100),
         }, TabButton)
